979ee5a3e095376ed417248279086f3a137350f2
Bernardo Pires Mesko <bernardo.mesko@ip5tecnologia.com.br>
2023-03-22 21:56:44 -0300

    Put ranking grid in main Ranking Page

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,34 +1,40 @@
-import React, {useState, useEffect } from 'react';
-import MovieImages from './MoveImages';
+import React, { useState, useEffect } from "react";
+import MovieImages from "./MoveImages";
+import RankingGrid from "./RankingGrid";
 
 export const RankItems = () => {
-    const [items, setItems] = useState([]);
-    // Type 1 = Movies, 2 = Albums
-    const dataType = 1;
+  const [items, setItems] = useState([]);
+  // Type 1 = Movies, 2 = Albums
+  const dataType = 1;
 
-    useEffect(() => {
-        // Makes an HTTP GET to the backend
-        fetch (`item/${dataType}`)
-        .then((results) => {
-            return results.json();
-        })
-        .then(data => {
-            setItems(data);
-        })
-    }, []);
+  useEffect(() => {
+    // Makes an HTTP GET to the backend
+    fetch(`item/${dataType}`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
 
-    return(
-        <main>
-            <div className='items-not-ranked'>
-            {
-                (items.length > 0) ? items.map(
-                    (item) => 
-                    <div className='unranked-cell'>
-                    <img id={`item-${item.id}`} src={MovieImages.find(o => o.id === item.imageId)?.image} />
-                    </div>
-                ) : <div>Loading...</div>
-            }
+  return (
+    <main>
+      <RankingGrid items={items} imageArray={MovieImages} />
+      <div className="items-not-ranked">
+        {items.length > 0 ? (
+          items.map((item) => (
+            <div className="unranked-cell">
+              <img
+                id={`item-${item.id}`}
+                src={MovieImages.find((o) => o.id === item.imageId)?.image}
+              />
             </div>
-        </main>
-    )
-}
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </main>
+  );
+};
